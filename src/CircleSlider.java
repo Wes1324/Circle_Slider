bb09d8d4d15126f7ccd419412ac15b82aefe7ad9
Wes1324 <xtbh_5@hotmail.com>
2017-05-26 07:01:27 +0100

    Program now requests 3 questions (one for each circle) and the name of user that will be answering the questions before it will run. CirclePanel's constructor now accepts a colour so the circles shown in the program are of different colours Use safer FileWriter constructor that takes File object User name, date, questions answered and their replies are now written to the excel spreadsheet.

--- src/CircleSlider.java
+++ src/CircleSlider.java
@@ -4,14 +4,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.io.FileWriter;
+import java.util.Date;
 
 public class CircleSlider {
 
     private JTextArea numberBox1;
     private JTextArea numberBox2;
     private JTextArea numberBox3;
-    private String circle1question;
+    private JTextField name;
+    private JTextField firstQ;
+    private JTextField secondQ;
+    private JTextField thirdQ;
 
     public static void main(String [] args) {
         new CircleSlider().go();
@@ -19,120 +24,153 @@ public class CircleSlider {
 
     private void go() {
 
-        circle1question = JOptionPane.showInputDialog("insert question to go above first circle");
+        name = new JTextField(15);
+        firstQ = new JTextField(15);
+        secondQ = new JTextField(15);
+        thirdQ = new JTextField(15);
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel dialogPanel = new JPanel();
+        BoxLayout dialogBoxLayout = new BoxLayout(dialogPanel,BoxLayout.Y_AXIS);
+        dialogPanel.setLayout(dialogBoxLayout);
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+        dialogPanel.add(new JLabel("Insert user's name:"));
+        dialogPanel.add(name);
+        dialogPanel.add(new JLabel("Insert first question:"));
+        dialogPanel.add(firstQ);
+        dialogPanel.add(new JLabel("Insert second question:"));
+        dialogPanel.add(secondQ);
+        dialogPanel.add(new JLabel("Insert third question:"));
+        dialogPanel.add(thirdQ);
 
-        JLabel q1 = new JLabel(circle1question);
-        q1.setPreferredSize(new Dimension(150,40));
-        q1.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+        int dialogSelection = JOptionPane.showConfirmDialog(null, dialogPanel,"Program set-up",JOptionPane.OK_CANCEL_OPTION);
 
-        JLabel q2 = new JLabel(circle1question);
-        q2.setPreferredSize(new Dimension(150,40));
-        q2.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+        if(dialogSelection == JOptionPane.OK_OPTION) {
 
-        JLabel q3 = new JLabel(circle1question);
-        q3.setPreferredSize(new Dimension(150,40));
-        q3.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        numberBox1 = new JTextArea("0");
-        numberBox1.setEditable(false);
-        numberBox1.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+            JPanel mainPanel = new JPanel(new BorderLayout(5,5));
+//			mainPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
 
-        numberBox2 = new JTextArea("0");
-        numberBox2.setEditable(false);
-        numberBox2.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+            JLabel q1 = new JLabel(firstQ.getText());
+            q1.setPreferredSize(new Dimension(150,40));
+//			q1.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
 
-        numberBox3 = new JTextArea("0");
-        numberBox3.setEditable(false);
-        numberBox3.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+            JLabel q2 = new JLabel(secondQ.getText());
+            q2.setPreferredSize(new Dimension(150,40));
+//			q2.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
 
-        JPanel topPanel1 = new JPanel();
-        topPanel1.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-        JPanel topPanel2 = new JPanel();
-        topPanel2.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-        JPanel topPanel3 = new JPanel();
-        topPanel3.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+            JLabel q3 = new JLabel(thirdQ.getText());
+            q3.setPreferredSize(new Dimension(150,40));
+//			q3.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
 
-        CirclePanel circleOne = new CirclePanel();
-        circleOne.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        circleOne.addMouseListener(new MyMouseListener(numberBox1, circleOne));
-        circleOne.addMouseMotionListener(new MyMouseListener(numberBox1, circleOne));
+            numberBox1 = new JTextArea("0");
+            numberBox1.setFont(numberBox1.getFont().deriveFont(Font.BOLD,14f));
+            numberBox1.setBackground(new Color(238,238,238));
+            numberBox1.setEditable(false);
+//			numberBox1.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
 
-        CirclePanel circleTwo = new CirclePanel();
-        circleTwo.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        circleTwo.addMouseListener(new MyMouseListener(numberBox2, circleTwo));
-        circleTwo.addMouseMotionListener(new MyMouseListener(numberBox2, circleTwo));
+            numberBox2 = new JTextArea("0");
+            numberBox2.setFont(numberBox2.getFont().deriveFont(Font.BOLD,14f));
+            numberBox2.setBackground(new Color(238,238,238));
+            numberBox2.setEditable(false);
+//			numberBox2.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
 
-        CirclePanel circleThree = new CirclePanel();
-        circleThree.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        circleThree.addMouseListener(new MyMouseListener(numberBox3, circleThree));
-        circleThree.addMouseMotionListener(new MyMouseListener(numberBox3, circleThree));
+            numberBox3 = new JTextArea("0");
+            numberBox3.setFont(numberBox3.getFont().deriveFont(Font.BOLD,14f));
+            numberBox3.setBackground(new Color(238,238,238));
+            numberBox3.setEditable(false);
+//			numberBox3.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
 
-        JButton submitButton = new JButton("Submit");
-        submitButton.addActionListener(new MyButtonListener());
+            JPanel topPanel1 = new JPanel();
+//			topPanel1.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+            JPanel topPanel2 = new JPanel();
+//			topPanel2.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+            JPanel topPanel3 = new JPanel();
+//			topPanel3.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 
-        JPanel panelOne = new JPanel();
-        panelOne.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-        JPanel panelTwo = new JPanel();
-        panelTwo.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-        JPanel panelThree = new JPanel();
-        panelThree.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            CirclePanel circleOne = new CirclePanel(Color.RED);
+//			circleOne.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            circleOne.addMouseListener(new MyMouseListener(numberBox1, circleOne));
+            circleOne.addMouseMotionListener(new MyMouseListener(numberBox1, circleOne));
 
-        BoxLayout BLOne = new BoxLayout(panelOne,BoxLayout.Y_AXIS);
-        BoxLayout BLTwo = new BoxLayout(panelTwo,BoxLayout.Y_AXIS);
-        BoxLayout BLThree = new BoxLayout(panelThree,BoxLayout.Y_AXIS);
+            CirclePanel circleTwo = new CirclePanel(Color.YELLOW);
+//			circleTwo.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            circleTwo.addMouseListener(new MyMouseListener(numberBox2, circleTwo));
+            circleTwo.addMouseMotionListener(new MyMouseListener(numberBox2, circleTwo));
 
-        panelOne.setLayout(BLOne);
-        panelTwo.setLayout(BLTwo);
-        panelThree.setLayout(BLThree);
+            CirclePanel circleThree = new CirclePanel(Color.BLUE);
+//			circleThree.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            circleThree.addMouseListener(new MyMouseListener(numberBox3, circleThree));
+            circleThree.addMouseMotionListener(new MyMouseListener(numberBox3, circleThree));
 
-        topPanel1.add(q1);
-        topPanel1.add(numberBox1);
-        topPanel2.add(q2);
-        topPanel2.add(numberBox2);
-        topPanel3.add(q3);
-        topPanel3.add(numberBox3);
+            JButton submitButton = new JButton("Submit");
+            submitButton.addActionListener(new MyButtonListener());
 
-        panelOne.add(topPanel1);
-        panelOne.add(circleOne);
+            JPanel panelOne = new JPanel();
+//			panelOne.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            JPanel panelTwo = new JPanel();
+//			panelTwo.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            JPanel panelThree = new JPanel();
+//			panelThree.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 
-        panelTwo.add(topPanel2);
-        panelTwo.add(circleTwo);
-//		panelTwo.add(submitButton);
+            BoxLayout BLOne = new BoxLayout(panelOne,BoxLayout.Y_AXIS);
+            BoxLayout BLTwo = new BoxLayout(panelTwo,BoxLayout.Y_AXIS);
+            BoxLayout BLThree = new BoxLayout(panelThree,BoxLayout.Y_AXIS);
 
-        panelThree.add(topPanel3);
-        panelThree.add(circleThree);
+            panelOne.setLayout(BLOne);
+            panelTwo.setLayout(BLTwo);
+            panelThree.setLayout(BLThree);
 
-        mainPanel.add(panelOne);
-        mainPanel.add(panelTwo);
-        mainPanel.add(panelThree);
-//		mainPanel.add(submitButton);
+            topPanel1.add(q1);
+            topPanel1.add(numberBox1);
+            topPanel2.add(q2);
+            topPanel2.add(numberBox2);
+            topPanel3.add(q3);
+            topPanel3.add(numberBox3);
 
-        frame.setContentPane(mainPanel);
+            panelOne.add(topPanel1);
+            panelOne.add(circleOne);
 
-        frame.pack();
-//		Rectangle screen = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-//		frame.setSize(screen.width, screen.height);
-        frame.setVisible(true);
-//		System.out.println(frame.getWidth() +", " + frame.getHeight());
-        frame.setResizable(false);
+            panelTwo.add(topPanel2);
+            panelTwo.add(circleTwo);
+//			panelTwo.add(submitButton);
 
+            panelThree.add(topPanel3);
+            panelThree.add(circleThree);
+
+            mainPanel.add(panelOne,BorderLayout.WEST);
+            mainPanel.add(panelTwo,BorderLayout.CENTER);
+            mainPanel.add(panelThree,BorderLayout.EAST);
+            mainPanel.add(submitButton,BorderLayout.SOUTH);
+
+            frame.setContentPane(mainPanel);
+
+            frame.pack();
+//			Rectangle screen = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+//			frame.setSize(screen.width, screen.height);
+            frame.setVisible(true);
+//			System.out.println(frame.getWidth() +", " + frame.getHeight());
+            frame.setResizable(false);
+        }
     }
 
     class MyButtonListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
             try {
-                FileWriter writer = new FileWriter("circleOutput.csv");
-                writer.write(circle1question + ",");
-                writer.write(numberBox1.getText());
-                writer.write(numberBox2.getText());
-                writer.write(numberBox3.getText());
+                File outputFile = new File("Circle outputs.csv");
+                FileWriter writer = new FileWriter(outputFile,true);
+
+                writer.write(name.getText() + "\n");
+
+                Date now = new Date();
+
+//				writer.write(String.format("%tc", now) + "\n");
+                writer.write(String.format("%ta %<te %<tb %<tY %<tT", now) + "\n");
+                writer.write(firstQ.getText() + "," + numberBox1.getText() + "\n");
+                writer.write(secondQ.getText() + "," + numberBox2.getText() + "\n");
+                writer.write(thirdQ.getText() + "," + numberBox3.getText() + "\n" +"\n");
                 writer.flush();
             } catch(Exception ex) {
                 System.out.println("Setting up writer and/or writing lines failed");
@@ -166,6 +204,10 @@ public class CircleSlider {
                 }
             }
         }
+
+//		public void mouseReleased(MouseEvent e) {
+//			circle.addDisplacements();
+//		}
     }
 }
 
@@ -173,31 +215,36 @@ class CirclePanel extends JPanel {
 
     private int xStart;
     private int yStart;
-    private int displacement;
+    private int displacement = 0;
     private int circleSize = 15;
     private int xCoord = 130;
     private int yCoord = 130;
+    private Color circleColor;
 
-    CirclePanel() {
+    CirclePanel(Color Colour) {
         setPreferredSize(new Dimension(280,280));
+        circleColor = Colour;
     }
 
     public void paintComponent(Graphics g) {
-        g.setColor(Color.WHITE);
+        g.setColor(new Color(238,238,238));
         g.fillRect(0,0,this.getWidth(),this.getHeight());
-        g.setColor(Color.GREEN);
+        g.setColor(circleColor);
         g.fillOval(xCoord,yCoord,circleSize,circleSize);
     }
 
     void setStart(double xS, double yS) {
+        System.out.println(xStart + ", " + yStart);
         xStart = (int) xS;
         yStart = (int) yS;
+        System.out.println(xStart + ", " + yStart);
     }
 
     int calcNewSize(double Xnow, double Ynow) {
         int currentX = (int) Xnow;
         int currentY = (int) Ynow;
         displacement = (int) Math.abs(Math.hypot(xStart-currentX, yStart-currentY));
+        System.out.println("displacement: " + displacement + ", circle size: " + circleSize);
         resizeAndCallRepaint();
         return displacement;
     }
